d59ffbc1aa63ebb26bca66fa3f592dd185d4b33f
Jennifer Lo <jenniferlo@ucsb.edu>
2022-05-01 15:07:34 -0700

    copied over my main code so far

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,294 @@
-#include <iostream>
+/*
+Allocore 
 
-// for master branch
-// #include "al/core.hpp"
+Description:
+Interact with paintbrush using ray intersection tests, and draw 3D spheres that are mapped sonically to 2D coordinates
 
-// for devel branch
+*/
+
 #include "al/app/al_App.hpp"
 #include "al/graphics/al_Shapes.hpp"
+#include "al/math/al_Random.hpp"
+#include "al/math/al_Ray.hpp"
+
+#include "Gamma/Analysis.h"
+#include "Gamma/Effects.h"
+#include "Gamma/Envelope.h"
+#include "Gamma/Oscillator.h"
+
+#include "al/scene/al_PolySynth.hpp"
+#include "al/scene/al_SynthSequencer.hpp"
+#include "al/ui/al_ControlGUI.hpp"
+#include "al/ui/al_Parameter.hpp"
+#include "al/io/al_Window.hpp"
+
+
+#include <vector>
 
 using namespace al;
 
-struct MyApp : App {
-  Mesh mesh;
-  double phase = 0;
+
+class Sound : public SynthVoice {
+ public:
+  // Unit generators
+  gam::Pan<> mPan;
+  gam::Sine<> mOsc;
+  gam::Env<3> mAmpEnv;
+  // envelope follower to connect audio output to graphics
+  gam::EnvFollow<> mEnvFollow;
+
+  // Additional members
+  Mesh mMesh;
+
+
+  // Initialize voice. This function will only be called once per voice when
+  // it is created. Voices will be reused if they are idle.
+  void init() override {
+    // Intialize envelope
+    mAmpEnv.curve(0);  // make segments lines
+    mAmpEnv.levels(0, 1, 1, 0);
+    mAmpEnv.sustainPoint(2);  // Make point 2 sustain until a release is issued
+
+    // We have the mesh be a sphere
+    //addSphere(mMesh, 0.02, 30);
+
+    // This is a quick way to create parameters for the voice. Trigger
+    // parameters are meant to be set only when the voice starts, i.e. they
+    // are expected to be constant within a voice instance. (You can actually
+    // change them while you are prototyping, but their changes will only be
+    // stored and aplied when a note is triggered.)
+ 
+    //(default val, min, max)
+    createInternalTriggerParameter("amplitude", 0.3, 0.0, 1.0);
+    createInternalTriggerParameter("frequency", 60, 20, 5000);
+    createInternalTriggerParameter("attackTime", 1.0, 0.01, 3.0);
+    createInternalTriggerParameter("releaseTime", 1.0, 0.1, 10.0);
+    createInternalTriggerParameter("pan", 0.0, -1.0, 1.0);
+    createInternalTriggerParameter("x", 0.0, 0.0, 1000.0);
+    createInternalTriggerParameter("y", 0.0, 0.0, 1000.0);
+    createInternalTriggerParameter("z", 80.0, 0.0, 100.0);
+  }
+
+  // The audio processing function
+  void onProcess(AudioIOData& io) override {
+    // Get the values from the parameters and apply them to the corresponding
+    // unit generators. You could place these lines in the onTrigger() function,
+    // but placing them here allows for realtime prototyping on a running
+    // voice, rather than having to trigger a new voice to hear the changes.
+    // Parameters will update values once per audio callback because they
+    // are outside the sample processing loop.
+    mOsc.freq(getInternalParameterValue("frequency"));
+    mAmpEnv.lengths()[0] = getInternalParameterValue("attackTime");
+    mAmpEnv.lengths()[2] = getInternalParameterValue("releaseTime");
+    mPan.pos(getInternalParameterValue("pan"));
+    while (io()) {
+      float s1 = mOsc() * mAmpEnv() * getInternalParameterValue("amplitude");
+      float s2;
+      mEnvFollow(s1);
+      mPan(s1, s1, s2);
+      io.out(0) += s1;
+      io.out(1) += s2;
+    }
+    // We need to let the synth know that this voice is done
+    // by calling the free(). This takes the voice out of the
+    // rendering chain
+    if (mAmpEnv.done() && (mEnvFollow.value() < 0.001f)) free();
+  }
+
+  // The triggering functions just need to tell the envelope to start or release
+  // The audio processing function checks when the envelope is done to remove
+  // the voice from the processing chain.
+  void onTriggerOn() override { mAmpEnv.reset(); }
+
+  void onTriggerOff() override { mAmpEnv.release(); }
+};
+
+struct RayBrush : App {
+
+  //variables for graphics
+  Material material;
+  Light light;
+  Mesh mMesh;
+
+  std::vector <Vec3f> pos;
+  std::vector <Color> colorSpheres;
+  Color colorPicker{1, 1, 1};
+
+
+  //variables for sound
+  SynthGUIManager<Sound> synthManager{"Sound"};
+  int midiNote = 0;
+  float frequency = 0;
 
   void onCreate() override {
-    addTetrahedron(mesh);
-    lens().near(0.1).far(25).fovy(45);
-    nav().pos(0, 0, 4);
-    nav().quat().fromAxisAngle(0. * M_2PI, 0, 1, 0);
+    //for graphics
+    nav().pos(0, 0, 80); //zoom in and out, higher z is out farther away
+    light.pos(0, 0, 80); // where the light is set
+    addSphere(mMesh, 0.4); 
+    mMesh.generateNormals();
+
+    //for sound
+    // disable nav control mouse drag to look
+    navControl().useMouse(false);
+
+    // Set sampling rate for Gamma objects from app's audio
+    gam::sampleRate(audioIO().framesPerSecond());
+    imguiInit();
+    synthManager.synthRecorder().verbose(true);
+  }
+
+  // The audio callback function. Called when audio hardware requires data
+  void onSound(AudioIOData& io) override {
+    synthManager.render(io);  // Render audio
   }
 
   void onAnimate(double dt) override {
-    double period = 10;
-    phase += dt / period;
-    if (phase >= 1.) phase -= 1.;
+    // The GUI is prepared here
+    navControl().useMouse(!isImguiUsingInput()); //allows screen to move with mouse
+
+    //navControl().active(!isImguiUsingInput());  // not sure what this does
+    
+    imguiBeginFrame();
+    ImGui::Begin("my window");
+    // Draw a window that contains the synth control panel
+    synthManager.drawSynthControlPanel();
+    
+    // Edit 3 floats representing a color
+    ImGui::ColorEdit3("clear color", colorPicker.components);
+    ImGui::End();
+    imguiEndFrame();
   }
 
-  void onDraw(Graphics& g) override {
-    g.clear(0, 0, 0);
-    g.polygonLine();
-    g.pushMatrix();
-    g.rotate(phase * 360, 0, 1, 0);
-    g.color(1);
-    g.draw(mesh);
-    g.popMatrix();
+
+  virtual void onDraw(Graphics &g) override {
+    g.clear(0);
+    gl::depthTesting(true);
+    g.lighting(true);
+    //std::cout<<"in onDraw "<< " pos[0]: "<< pos[0] <<std::endl;
+    //draw and color spheres 
+    for (int i = 0; i < pos.size(); i++) {
+      //std::cout<<"in onDraw, i = " << i << " pos: "<< pos[i] << "color = " <<colorSpheres[i].y<<std::endl;
+      g.pushMatrix();
+      g.translate(pos[i]);
+      //g.color(1, .87, .5);
+      g.color(colorSpheres[i]);
+      g.draw(mMesh);
+      g.popMatrix();
+    }
+    // Render the synth's graphics
+    synthManager.render(g);
+    // GUI is drawn here
+    imguiDraw();
+    //change z coord with gui
+    nav().pos(0, 0, synthManager.voice()->getInternalParameterValue("z")); //zoom in and out, higher z is out farther away
+    light.pos(0, 0, synthManager.voice()->getInternalParameterValue("z")); // where the light is set
+    
+    
+
   }
+
+  Vec3d unproject(Vec3d screenPos) {
+    auto &g = graphics();
+    auto mvp = g.projMatrix() * g.viewMatrix() * g.modelMatrix();
+    Matrix4d invprojview = Matrix4d::inverse(mvp);
+    Vec4d worldPos4 = invprojview.transform(screenPos);
+    return worldPos4.sub<3>(0) / worldPos4.w;
+  }
+
+  Rayd getPickRay(int screenX, int screenY) {
+    Rayd r;
+    Vec3d screenPos;
+    screenPos.x = (screenX * 1. / width()) * 2. - 1.;
+    screenPos.y = ((height() - screenY) * 1. / height()) * 2. - 1.;
+    screenPos.z = -1.;
+    Vec3d worldPos = unproject(screenPos);
+    r.origin().set(worldPos);
+
+    screenPos.z = 1.;
+    worldPos = unproject(screenPos);
+    r.direction().set(worldPos);
+    r.direction() -= r.origin();
+    r.direction().normalize();
+    return r;
+  }
+
+  bool onMouseMove(const Mouse &m) override {
+    // intersect ray with each sphere in scene
+
+    return true;
+  }
+  bool onMouseDown(const Mouse &m) override {
+    // if mouse clicks on image gui, do not draw!!!
+    if(isImguiUsingInput()){
+      return true;
+    }
+    Vec3d screenPos;
+    screenPos.x = (m.x() * 1. / width()) * 2. - 1.;
+    screenPos.y = ((height() - m.y()) * 1. / height()) * 2. - 1.;
+    screenPos.z = 1.;
+    Vec3d worldPos = unproject(screenPos);
+    //add a sphere to plane
+    Vec3f position = Vec3f(worldPos.x, worldPos.y, worldPos.z);
+    //std::cout<<"postion = "<< position <<std::endl;
+
+    pos.push_back(position);
+
+
+    //trigger note on
+    //set x and y coord on gui
+    synthManager.voice()->setInternalParameterValue("x",m.x());
+    synthManager.voice()->setInternalParameterValue("y",m.y());
+
+    // trigger note on
+    const float A4 = 220.f;
+    midiNote = (m.x()+m.y())%87 + 21; //(range from 21-108)
+    std::cout<<"Drawing midi note = "<< midiNote <<std::endl;
+    //std::cout<<"currentSphereCount = "<< currentSphereCount <<std::endl;
+
+    if (midiNote > 0) {
+        synthManager.voice()->setInternalParameterValue(
+            "frequency", ::pow(2.f, (midiNote - 69.f) / 12.f) * A4);
+        synthManager.triggerOn(midiNote);
+      }
+    Color sphereColor = colorPicker;
+    colorSpheres.push_back(sphereColor);
+
+    
+    return true;
+  }
+  bool onMouseDrag(const Mouse &m) override {
+    // if mouse drags on image gui, do not draw!!!
+    if(isImguiUsingInput()){
+      return true;
+    }
+
+    Vec3d screenPos;
+    screenPos.x = (m.x() * 1. / width()) * 2. - 1.;
+    screenPos.y = ((height() - m.y()) * 1. / height()) * 2. - 1.;
+    screenPos.z = 1.;
+    Vec3d worldPos = unproject(screenPos);
+    //add a sphere to plane
+    Vec3f position = Vec3f(worldPos.x, worldPos.y, worldPos.z);
+    //std::cout<<"postion = "<< position <<std::endl;
+
+    pos.push_back(position);
+    Color sphereColor = colorPicker;
+    colorSpheres.push_back(sphereColor);
+  
+    return true;
+  }
+
+  bool onMouseUp(const Mouse &m) override {
+    //trigger note off
+    synthManager.triggerOff(midiNote);
+    return true;
+  }
+
+  //use sequencncer->synthManager()
+  //count duration of mouse sequence and add duration with voice after to make chords
 };
-
 int main() {
-  MyApp app;
-  app.dimensions(600, 400);
+  RayBrush app;
   app.start();
+  return 0;
 }
