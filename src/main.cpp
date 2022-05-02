58809085af0ac1f874766a36e80c533d29444b73
Jennifer Lo <jenniferlo@ucsb.edu>
2022-05-01 21:15:38 -0700

    working on adding duration to loop sound

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@ Interact with paintbrush using ray intersection tests, and draw 3D spheres that 
 #include "al/ui/al_ControlGUI.hpp"
 #include "al/ui/al_Parameter.hpp"
 #include "al/io/al_Window.hpp"
+#include "al/system/al_Time.hpp"
 
 
 #include <vector>
@@ -28,82 +29,6 @@ Interact with paintbrush using ray intersection tests, and draw 3D spheres that 
 
 using namespace al;
 
-/*
-class Sound : public SynthVoice {
- public:
-  // Unit generators
-  gam::Pan<> mPan;
-  gam::Sine<> mOsc;
-  gam::Env<3> mAmpEnv;
-  // envelope follower to connect audio output to graphics
-  gam::EnvFollow<> mEnvFollow;
-
-  // Additional members
-  Mesh mMesh;
-
-
-  // Initialize voice. This function will only be called once per voice when
-  // it is created. Voices will be reused if they are idle.
-  void init() override {
-    // Intialize envelope
-    mAmpEnv.curve(0);  // make segments lines
-    mAmpEnv.levels(0, 1, 1, 0);
-    mAmpEnv.sustainPoint(2);  // Make point 2 sustain until a release is issued
-
-    // We have the mesh be a sphere
-    //addSphere(mMesh, 0.02, 30);
-
-    // This is a quick way to create parameters for the voice. Trigger
-    // parameters are meant to be set only when the voice starts, i.e. they
-    // are expected to be constant within a voice instance. (You can actually
-    // change them while you are prototyping, but their changes will only be
-    // stored and aplied when a note is triggered.)
- 
-    //(default val, min, max)
-    createInternalTriggerParameter("amplitude", 0.3, 0.0, 1.0);
-    createInternalTriggerParameter("frequency", 60, 20, 5000);
-    createInternalTriggerParameter("attackTime", 1.0, 0.01, 3.0);
-    createInternalTriggerParameter("releaseTime", 1.0, 0.1, 10.0);
-    createInternalTriggerParameter("pan", 0.0, -1.0, 1.0);
-    createInternalTriggerParameter("x", 0.0, 0.0, 1000.0);
-    createInternalTriggerParameter("y", 0.0, 0.0, 1000.0);
-    createInternalTriggerParameter("z", 80.0, 0.0, 100.0);
-  }
-
-  // The audio processing function
-  void onProcess(AudioIOData& io) override {
-    // Get the values from the parameters and apply them to the corresponding
-    // unit generators. You could place these lines in the onTrigger() function,
-    // but placing them here allows for realtime prototyping on a running
-    // voice, rather than having to trigger a new voice to hear the changes.
-    // Parameters will update values once per audio callback because they
-    // are outside the sample processing loop.
-    mOsc.freq(getInternalParameterValue("frequency"));
-    mAmpEnv.lengths()[0] = getInternalParameterValue("attackTime");
-    mAmpEnv.lengths()[2] = getInternalParameterValue("releaseTime");
-    mPan.pos(getInternalParameterValue("pan"));
-    while (io()) {
-      float s1 = mOsc() * mAmpEnv() * getInternalParameterValue("amplitude");
-      float s2;
-      mEnvFollow(s1);
-      mPan(s1, s1, s2);
-      io.out(0) += s1;
-      io.out(1) += s2;
-    }
-    // We need to let the synth know that this voice is done
-    // by calling the free(). This takes the voice out of the
-    // rendering chain
-    if (mAmpEnv.done() && (mEnvFollow.value() < 0.001f)) free();
-  }
-
-  // The triggering functions just need to tell the envelope to start or release
-  // The audio processing function checks when the envelope is done to remove
-  // the voice from the processing chain.
-  void onTriggerOn() override { mAmpEnv.reset(); }
-
-  void onTriggerOff() override { mAmpEnv.release(); }
-};
-*/
 
 struct RayBrush : App {
 
@@ -116,13 +41,17 @@ struct RayBrush : App {
   std::vector <Color> colorSpheres;
   Color colorPicker{1, 1, 1};
   bool move_with_mouse = false;
-  
-  std::vector <int> last_stroke_positions; // keeps track of where all the stroke positions start for the undo function 
+  bool loop = false;
+
+  std::vector <int> midiNotes; // keeps track of the notes for each stroke
+  std::vector <float> durations; // keeps track of the durations for each stroke
+  std::vector <int> start_stroke_positions; // keeps track of where all the stroke positions start for the undo function 
 
   //variables for sound
   SynthGUIManager<Sound> synthManager{"Sound"};
   int midiNote = 0;
   float frequency = 0;
+  Timer durationTimer;
 
   void onCreate() override {
     //for graphics
@@ -162,7 +91,19 @@ struct RayBrush : App {
     ImGui::Checkbox("Move With Mouse", &move_with_mouse);
     if (move_with_mouse) {
       navControl().useMouse(!isImguiUsingInput()); //allows screen to move with mouse
-     }
+    }
+    ImGui::Checkbox("Turn on Loop", &loop); //turn on loop for note to keep repeating after being drawn 
+    if(loop){
+      double duration = durations.back(); //get duration
+      const float A4 = 220.f;
+      int noteToLoop = midiNotes.back();
+      //keep playing notes for a certain duration each time until loop is checked off
+      synthManager.voice()->setInternalParameterValue(
+            "frequency", ::pow(2.f, (noteToLoop - 69.f) / 12.f) * A4);
+      synthManager.triggerOn(noteToLoop);
+      synthManager.synthSequencer().addVoice(synthManager.voice(), 0.f, duration);
+    }
+
     if (ImGui::Button("Clear Drawing")) {
         // Buttons return true when clicked
         // clears the screen
@@ -172,10 +113,11 @@ struct RayBrush : App {
     if (ImGui::Button("Undo")) {
         // undoes the most recent stroke from last on mouse down
         // erase the last elements from last_stroke_position to end:
-        int getPrevStroke = last_stroke_positions.back();
-        last_stroke_positions.pop_back();
+        int getPrevStroke = start_stroke_positions.back();
+        start_stroke_positions.pop_back();
         pos.erase(pos.begin() + getPrevStroke, pos.end());
         colorSpheres.erase(colorSpheres.begin() + getPrevStroke, colorSpheres.end());
+        midiNotes.pop_back(); //remove associated midinote
 
     }
     ImGui::End();
@@ -221,6 +163,8 @@ struct RayBrush : App {
     if(isImguiUsingInput()){
       return true;
     }
+    //start duration timer
+    durationTimer.start();
 
     Vec3d screenPos;
     screenPos.x = (m.x() * 1. / width()) * 2. - 1.;
@@ -231,9 +175,8 @@ struct RayBrush : App {
     Vec3f position = Vec3f(worldPos.x, worldPos.y, worldPos.z);
     //std::cout<<"postion = "<< position <<std::endl;
 
-    last_stroke_positions.push_back(pos.size()); //set the last stroke positon to start from here
+    start_stroke_positions.push_back(pos.size()); //set the last stroke positon to start from here
     pos.push_back(position);
-    
 
 
     //trigger note on
@@ -254,6 +197,7 @@ struct RayBrush : App {
       }
     Color sphereColor = colorPicker;
     colorSpheres.push_back(sphereColor);
+    midiNotes.push_back(midiNote);
     
     return true;
   }
@@ -279,8 +223,11 @@ struct RayBrush : App {
   }
 
   bool onMouseUp(const Mouse &m) override {
+    durationTimer.stop();
+    durations.push_back(durationTimer.elapsedSec());
     //trigger note off
     synthManager.triggerOff(midiNote);
+
     return true;
   }
 
